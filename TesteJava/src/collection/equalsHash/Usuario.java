8f2bf5e5bd640b67915489f8b9864eb20d7f0d6d
Gabrielle Beus <gabriellebeus9@gmail.com>
2025-07-03 11:39:53 -0300

    EqualsHash

---
+++ TesteJava/src/collection/equalsHash/Usuario.java
@@ -0,0 +1,55 @@
+package collection.equalsHash;
+
+public class Usuario {
+    private String nome;
+    private String email;
+
+    public Usuario() {
+    }
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+    
+}
